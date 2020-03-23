0a4ac957bc214d5edbc8e21de95789e962cbfa75
Alex Hanson <alexhanson@alexs-air.lan>
2020-03-23 09:44:04 -0700

    rename refactor code

--- getItTicket/urls.py
+++ getItTicket/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tickets/', include('tickets.urls')),
+    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
