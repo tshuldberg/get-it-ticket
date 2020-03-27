4add53171bb194905a6212f6b382cdd4766425a0
Trey Shuldberg <trey@Treys-MBP.attlocal.net>
2020-03-26 18:02:11 -0700

    admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Venue, Event, Business
 # Register your models here.
+admin.site.register(Venue)
+admin.site.register(Business)
+admin.site.register(Event)
